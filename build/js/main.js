830f158c10004da452439f97f8938eaa5fa60b8f
admin <huynq@ichiba.vn>
2022-09-14 14:51:51 +0700

    - update

--- build/js/main.js
+++ build/js/main.js
@@ -1,95 +1,20 @@
-function initSwiperProduct(container, slidesPerView, spaceBetween, slidesPerColumn, slidesPerColumnFill) {
-    var swiper = new Swiper('#' + container + ' ' + '.swiper', {
-        slidesPerView: 1,
-        spaceBetween: spaceBetween,
-        slidesPerColumn: slidesPerColumn,
-        grabCursor: true,
-        slidesPerColumnFill: slidesPerColumnFill,
-        pagination: {
-            el: '#' + container + ' ' + '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '#' + container + ' ' + '.swiper-button-next',
-            prevEl: '#' + container + ' ' + '.swiper-button-prev',
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 1,
-            },
-            1024: {
-                slidesPerView: slidesPerView,
-            },
-        },
-        autoplay: {
-            delay: 3500,
-            disableOnInteraction: false,
-        },
-    });
-}
-
-function swiperCommon() {
-    $('.swiper_common').each(function () {
-        var container = $(this).attr('id');
-        var numberItem = parseInt($(this).attr('data-item'));
-        var space = parseInt($(this).attr('data-space'));
-        var column = parseInt($(this).attr('data-column'));
-        var columnFill = parseInt($(this).attr('data-column-fill'));
-        initSwiperProduct(container, numberItem, space, column, columnFill);
-    });
-}
-
-
-function openNavigation() {
-    //Toggle Navigation
-    /*Nav icon mobile*/
-    $('#navtop_icon').click(function () {
-        $(this).toggleClass('open');
-        $('.navtop_collapse').toggleClass('open');
-        $('.overlay').toggleClass('open');
-        $('html , body').toggleClass('open_menu');
-    });
-
-    /* When user clicks outside */
-    $(".overlay").click(function () {
-        $(this).toggleClass('open');
-        $("#navtop_icon").toggleClass("open");
-        $(".navtop_collapse").toggleClass("open");
-        $('html , body').toggleClass('open_menu');
-    });
-}
-
-function backToTop() {
-    $("a.back_to_top, .back_top_bar").click(function () {
-        $("html, body").animate({scrollTop: 0}, "fast");
-        return false;
-    });
-}
-//
-// function preloader() {
-//     var browserWindow = $(window);
-//
-//     // :: 1.0 Preloader Active Code
-//     browserWindow.on('load', function () {
-//         $('.preloader').fadeOut('slow', function () {
-//             $(this).remove();
-//         });
-//     });
-// }
-
-
-
 $(document).ready(function () {
-    AOS.init();
-    openNavigation();
-    swiperCommon();
-    // preloader();
-    backToTop();
-    // $(window).scroll(function(){
-    //     var sticky = $('#header'),
-    //         scroll = $(window).scrollTop();
-    //
-    //     if (scroll >= 100) sticky.addClass('fixed');
-    //     else sticky.removeClass('fixed');
-    // });
+
+
+    $("#main").onepage_scroll({
+        sectionContainer: ".section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+        easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+        animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+        pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+        updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+        beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+        afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+        loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+        keyboard: true,                  // You can activate the keyboard controls
+        responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+        // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+        // the browser's width is less than 600, the fallback will kick in.
+        direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
+    });
 })
