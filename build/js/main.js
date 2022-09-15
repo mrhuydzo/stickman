d87081b51f320518db3a833d0c288499705461df
admin <huynq@ichiba.vn>
2022-09-15 11:38:01 +0700

    - update

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-
     $("#main").onepage_scroll({
         sectionContainer: ".section",     // sectionContainer accepts any kind of selector in case you don't want to use section
         easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
